71a68a4e912e3df9a0f030582176842407c82087
Kamaal Farah <kamaal.f1@gmail.com>
2025-01-11 19:09:56 +0100

    Adding all updated files to danger message

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,10 @@
 import { message, danger } from "danger";
 
-const modifiedPythonFiles = danger.git.modified_files
-  .filter((filename) => filename.endsWith(".py"))
-  .join("- ");
-message("🐸🐸🐸 Changed Python files in this PR: \n - " + modifiedPythonFiles);
+const updatedPythonFiles = danger.git.created_files
+  .concat(danger.git.modified_files)
+  .filter((filename) => filename.endsWith(".py"));
+
+message(
+  "🐸🐸🐸 Changed Python files in this PR: \n - " +
+    updatedPythonFiles.join("- ")
+);
